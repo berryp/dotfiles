5c62558299dcc81a3fe9f5b3e6f962d42057f5aa
Berry Phillips <berryphillips@gmail.com>
2017-01-03 17:46:59 +0000

    Updates.

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -2,7 +2,7 @@
 
 echo "--- Updating system ---"
 sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
 
 

f75e32b792dc6276ecc203dfffd5a5114258015f
Berry Phillips <berryphillips@gmail.com>
2017-01-31 16:43:43 +0000

    Updates.

---
+++ install-lite.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+WORKDIR=$PWD
+
+function gitclone {
+  if [ ! -d $2 ]; then
+    git clone $1 $2
+  else
+    cd $2
+    git pull
+  fi
+
+  cd $WORKDIR
+}
+
+echo "--- Installing OhMyZSH ---"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "--- Installing vim-plug ---"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "--- Installing tmux plugin manager ---"
+gitclone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "--- Installing base16-shell ---"
+gitclone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+echo "--- Installing dotfiles ---"
+
+files=$(find . -maxdepth 1 -type f -name ".*"  $(printf "! -name %s " .gitignore .gitmodules))
+echo $files
+mkdir -p .tmp
+
+echo "--- Linking dotfiles ---"
+for file in $files; do
+    f=${file#./}
+    echo "Backing up $f."
+    mv $HOME/$f .tmp/
+    echo "Linking $f."
+    ln -s $WORKDIR/$f ~/$f
+done
+
+echo "--- Caveats ---"
+echo "Install VIM plugins using :PlugInstall"
+# vim -u NONE +PlugInstall +qall
